679d6098a47c4365df3c2028585f7147e91cd872
SyedHassaanali <syedhassaanalishah105@gmail.com>
2024-10-30 01:17:09 -0700

    budgetfoods

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("I will tell you which restaurant is best for your budget")
+var userinput = prompt("Enter your buget")
+
+if(userinput < 3000){
+    console.log("Gevini Foods")
+}
+else if (userinput < 5000){
+    console.log("Shaheen Shinwari")
+}
+else if (userinput < 10000){
+    console.log("Kabab jee")
+}
+ else {
+    console.log("Please type your buget under ten thousand")
+ }
+
+
+
+
+
+
