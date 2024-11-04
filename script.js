4c655bb0a4a50e09abd66ddc8a43bc390a0f0b9d
Syed Hassaan Ali <164741639+SyedHassaanAli105@users.noreply.github.com>
2024-11-04 14:24:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ alert("I will tell you which restaurant is best for your budget")
 var userinput = prompt("Enter your buget")
 
 if(userinput < 3000){
-    console.log("Gevini Foods")
+    alert("Gevini Foods")
 }
 else if (userinput < 5000){
-    console.log("Shaheen Shinwari")
+     alert("Shaheen Shinwari")
 }
 else if (userinput < 10000){
-    console.log("Kabab jee")
+     alert("Kabab jee")
 }
  else {
-    console.log("Please type your buget under ten thousand")
+     alert("Please type your buget under ten thousand")
  }
 
 
